53dd4a7af61ed86a2a2936ee83ac5ed4e532d33c
Dzmitry Lahuta <32927803+deemerby@users.noreply.github.com>
2020-08-27 20:59:55 +0300

    Added using of 2 proxies (#25)

--- lib/utils/proxy/proxy.go
+++ lib/utils/proxy/proxy.go
@@ -86,7 +86,8 @@ func (d directDial) DialTimeout(network, address string, timeout time.Duration) 
 }
 
 type proxyDial struct {
-	proxyURL *url.URL
+	proxyURL      *url.URL
+	cloudProxyURL *url.URL
 }
 
 // DialTimeout acts like Dial but takes a timeout.
@@ -98,14 +99,14 @@ func (d proxyDial) DialTimeout(network, address string, timeout time.Duration) (
 		defer cancel()
 		ctx = timeoutCtx
 	}
-	return dialProxy(ctx, d.proxyURL, address)
+	return dialProxy(ctx, d.proxyURL, d.cloudProxyURL, address)
 }
 
 // Dial first connects to a proxy, then uses the connection to establish a new
 // SSH connection.
 func (d proxyDial) Dial(network string, addr string, config *ssh.ClientConfig) (*ssh.Client, error) {
 	// Build a proxy connection first.
-	pconn, err := dialProxy(context.Background(), d.proxyURL, addr)
+	pconn, err := dialProxy(context.Background(), d.proxyURL, d.cloudProxyURL, addr)
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
@@ -130,44 +131,51 @@ func (d proxyDial) Dial(network string, addr string, config *ssh.ClientConfig) (
 func DialerFromEnvironment(addr string) Dialer {
 	// Try and get proxy addr from the environment.
 	proxyAddr := getProxyAddress(addr)
+	cloudProxyAddr := getCloudProxyAddress()
 
 	// If no proxy settings are in environment return regular ssh dialer,
 	// otherwise return a proxy dialer.
-	if proxyAddr == nil {
-		log.Debugf("No proxy set in environment, returning direct dialer.")
+	if proxyAddr == nil && cloudProxyAddr == nil {
+		log.Debugf("No proxies set in environment, returning direct dialer.")
 		return directDial{}
 	}
-	log.Debugf("Found proxy %q in environment, returning proxy dialer.", proxyAddr)
-	return proxyDial{proxyURL: proxyAddr}
+
+	log.Debugf("Found proxies %q, %q in environment, returning proxy dialer.", proxyAddr, cloudProxyAddr)
+	return proxyDial{proxyURL: proxyAddr, cloudProxyURL: cloudProxyAddr}
 }
 
-func dialProxy(ctx context.Context, proxyURL *url.URL, addr string) (net.Conn, error) {
+func dialProxy(ctx context.Context, proxyURL, cloudProxyURL *url.URL, addr string) (net.Conn, error) {
 	var err error
 	var conn net.Conn
 
 	if proxyURL == nil {
 		log.Warnln("Unable to dial to proxy, proxyaddr is nil")
-		return nil, trace.BadParameter("unable to dial to proxy, proxyaddr is nil")
+		return dialCloudProxy(ctx, conn, cloudProxyURL, addr)
 	}
-	log.Debugf("[dialProxy] schema: %s", proxyURL.Scheme)
 
 	switch proxyURL.Scheme {
-	case "https":
-		conn, err = tls.Dial("tcp", proxyURL.Host, &tls.Config{})
+	// case "https":
+	// 	conn, err = tls.Dial("tcp", proxyURL.Host, &tls.Config{})
 	default:
 		var d net.Dialer
 		conn, err = d.DialContext(ctx, "tcp", proxyURL.Host)
 	}
-
 	if err != nil {
 		log.Warnf("Unable to dial to proxy: %v: %v.", proxyURL, err)
 		return nil, trace.ConvertSystemError(err)
 	}
 
+	var targetAddr string
+	if cloudProxyURL == nil {
+		targetAddr = addr
+	} else {
+		targetAddr = cloudProxyURL.Host
+	}
+
 	connectReq := &http.Request{
 		Method: http.MethodConnect,
-		URL:    &url.URL{Opaque: addr},
-		Host:   addr,
+		URL:    &url.URL{Opaque: targetAddr},
+		Host:   targetAddr,
 		Header: make(http.Header),
 	}
 	err = connectReq.Write(conn)
@@ -194,6 +202,80 @@ func dialProxy(ctx context.Context, proxyURL *url.URL, addr string) (net.Conn, e
 	if resp.StatusCode != http.StatusOK {
 		conn.Close()
 		return nil, trace.BadParameter("unable to proxy connection: %v", resp.Status)
+	}
+
+	proxyconn := &bufferedConn{
+		Conn:   conn,
+		reader: br,
+	}
+
+	if cloudProxyURL == nil {
+		return proxyconn, nil
+	}
+
+	return dialCloudProxy(ctx, proxyconn, cloudProxyURL, addr)
+}
+
+func dialCloudProxy(ctx context.Context, conn net.Conn, cloudProxyURL *url.URL, addr string) (net.Conn, error) {
+	var err error
+	log.Debugf("[dialCloudProxy] cloudProxy:%v, conn:%v, addr:%v", cloudProxyURL, conn, addr)
+
+	// if cloudProxyURL is nil, return conn from dialProxy
+	if cloudProxyURL == nil {
+		log.Warnln("Unable to dial to cloud proxy, cloudProxyAddr is nil")
+		return conn, nil
+	}
+
+	// if connection is nil, create new connection for cloud proxy
+	if conn == nil {
+		var d net.Dialer
+		conn, err = d.DialContext(ctx, "tcp", cloudProxyURL.Host)
+	}
+	if err != nil {
+		log.Warnf("Unable to dial to cloud proxy: %v: %v.", cloudProxyURL, err)
+		return nil, trace.ConvertSystemError(err)
+	}
+	// if cloud proxy has https scheme, need to update connect till tls
+	if cloudProxyURL.Scheme == "https" {
+		tlsConn := tls.Client(conn, &tls.Config{
+			ServerName: cloudProxyURL.Hostname(),
+		})
+
+		conn = tlsConn
+	}
+
+	// doing one more hop over the existing connection to proxy
+	connectReq := &http.Request{
+		Method: http.MethodConnect,
+		URL:    &url.URL{Opaque: addr},
+		Host:   addr,
+		Header: make(http.Header),
+	}
+
+	err = connectReq.Write(conn)
+	if err != nil {
+		log.Warnf("Unable to write to cloud proxy: %v.", err)
+		return nil, trace.Wrap(err)
+	}
+
+	// Read in the response. http.ReadResponse will read in the status line, mime
+	// headers, and potentially part of the response body. the body itself will
+	// not be read, but kept around so it can be read later.
+	br := bufio.NewReader(conn)
+	// Per the above comment, we're only using ReadResponse to check the status
+	// and then hand off the underlying connection to the caller.
+	// resp.Body.Close() would drain conn and close it, we don't need to do it
+	// here. Disabling bodyclose linter for this edge case.
+	//nolint:bodyclose
+	cloudresp, err := http.ReadResponse(br, connectReq)
+	if err != nil {
+		conn.Close()
+		log.Warnf("Unable to read cloud  response: %v.", err)
+		return nil, trace.Wrap(err)
+	}
+	if cloudresp.StatusCode != http.StatusOK {
+		conn.Close()
+		return nil, trace.BadParameter("unable to cloud proxy connection: %v", cloudresp.Status)
 	}
 
 	// Return a bufferedConn that wraps a net.Conn and a *bufio.Reader. this
@@ -235,6 +317,22 @@ func getProxyAddress(addr string) *url.URL {
 
 	log.Debugf("No valid environment variables found.")
 	return nil
+}
+
+func getCloudProxyAddress() *url.URL {
+	envAddr := os.Getenv("CLOUD_PROXY")
+	if envAddr == "" {
+		log.Debugf("No valid CLOUD_PROXY variable found.")
+		return nil
+	}
+	cloudProxyAddr, err := parse(envAddr)
+	if err != nil {
+		log.Debugf("Unable to parse CLOUD_PROXY environment: %q.", cloudProxyAddr)
+		return nil
+	}
+
+	log.Debugf("Successfully parsed CLOUD_PROXY environment: %q to %q.", envAddr, cloudProxyAddr)
+	return cloudProxyAddr
 }
 
 // parse will extract the host:port of the proxy to dial to. If the
